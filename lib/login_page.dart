abbb579d337f1ca6796028405f082664a93b338e
1832-begin <luanacabral430@gmail.com>
2021-05-23 20:14:36 -0400

    aula paginas

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'home_page.dart';
+
 class LoginPage extends StatefulWidget {
   LoginPage({Key key}) : super(key: key);
 
@@ -8,25 +10,59 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  String email = "";
+  String senha = "";
   @override
   Widget build(BuildContext context) {
-    return Material(
-      child: SizedBox(
-        width: double.infinity,
-        height: double.infinity,
-        child: Padding(
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: SizedBox(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          child: Padding(
             padding: const EdgeInsets.all(8.0),
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 TextField(
+                  onChanged: (text) => email = text,
+                  keyboardType: TextInputType.emailAddress,
                   decoration: InputDecoration(
                     labelText: 'Email',
                     border: OutlineInputBorder(),
                   ),
                 ),
+                SizedBox(
+                  height: 25,
+                ),
+                TextField(
+                  onChanged: (text) => senha = text,
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    labelText: 'Senha',
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+                SizedBox(
+                  height: 25,
+                ),
+                ElevatedButton(
+                    onPressed: () {
+                      if (email == 'luanacabral430@gmail.com' &&
+                          senha == '123') {
+                        print('login correto');
+                        Navigator.of(context).push(
+                          MaterialPageRoute(builder: (context) => HomePage()),
+                        );
+                      } else {
+                        print('login errado');
+                      }
+                    },
+                    child: Text('LOGIN'))
               ],
-            )),
+            ),
+          ),
+        ),
       ),
     );
   }
